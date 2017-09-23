b5317393a40739a956140bb158c1849f9818c520
Charlie S. Aka CS <hsmchul@hotmail.com>
2017-09-22 21:11:44 -0500

    Initial Commit

---
+++ GDCProject/Assets/Scripts/PlayerMovementBehaviour.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * GDC Intro to Unity
+ * 
+ * Character Movement by translation
+ * 
+ */
+
+[AddComponentMenu("GDC/Character/MoveByTranslate")]
+public class PlayerMovementBehaviour : MonoBehaviour
+{
+    /* In order to move a character by updating position, you need player's position vector.
+     * You can have it saved by declaring as variable, or can call everytime when frame is
+     * updated. We will declare Vector variable.
+     */
+    private Vector3 player_position;
+
+    /* We want to adjust player's movement speed within the editor; to do so, we declare speed variable
+     * as public. There are two ways of having public variables displayed in the editor; as textfield
+     * or as textfield with limit slider. We will use limit slider.
+     */
+    [Range(1.0f, 10.0f)]
+    public float player_speed = 1.0f;
+
+    private void Awake()
+    {
+        /* Set player_position vector as this game object's initial position */
+        player_position = this.transform.position;
+    }
+
+    private void Update()
+    {
+        /* To check if player has input any movement keys, we take in key input. There are two ways of doing this.
+         * One way is to get input as axis input; either X, Y, or Z axis.
+         * The other way to get input is to take in iput as keyboard input.
+         * For this script, we will use Input.GetAxis().
+         */
+        float horizontal_input = Input.GetAxis("Horizontal");
+        float vertical_input = Input.GetAxis("Vertical");
+
+        /* From here, we can calculate new X and Z position of player (We are ignoring Y axis)
+         */
+        //float player_x_pos = horizontal_input * player_speed;
+        //float player_y_pos = 0;
+        //float player_z_pos = vertical_input * player_speed;
+
+        /* Then apply new position to player's position vector
+         */
+        //player_position.Set(player_x_pos, player_y_pos, player_z_pos);
+
+        /* When we use Translate(), it means we are adding increment value to our game object's position vector.
+         */
+        //this.transform.Translate(player_position);
+
+        /* By this point, we notice our character does not move the way we want; it moves too fast. This is because our
+         * frame rate is high, its updating at a rate of 30~60FPS. To compensate this, we apply Time.deltaTime. DeltaTime
+         * is time between each frame, allowing us to smooth out any non-physics movements. Comment out pervious player
+         * position and use new code below.
+         */
+        float player_x_pos = horizontal_input * player_speed * Time.deltaTime;
+        float player_z_pos = vertical_input * player_speed * Time.deltaTime;
+
+        player_position = new Vector3(player_x_pos, 0, player_z_pos);
+
+        this.transform.Translate(player_position);
+    }
+}
---
+++ GDCProject/Assets/Scripts/PlayerMovementPhysicsBehaviour.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * GDC Intro to Unity
+ * 
+ * Moving object by force
+ * 
+ */
+
+[AddComponentMenu("GDC/Character/MoveByPhysics")]
+public class PlayerMovementPhysicsBehaviour : MonoBehaviour
+{
+    /* Instead of setting how fast do we want to move(velocity), we now set how hard do we want to add
+     * force to our rigidbody. So we change our variable name to force_applied.
+     */
+    [Range(1.0f, 10.0f)]
+    public float force_applied = 1.0f;
+
+    /* In order to access public functions within Rigidbody object, we need to get instantiated object
+     * from our game object; which means we need a variable that saves Rigidbody object.
+     */
+    private Rigidbody body;
+
+    private void Awake()
+    {
+        /* Link the instantiated rigidbody object
+         */
+        body = this.GetComponent<Rigidbody>();
+    }
+
+    /* Remember, we are applying physical force to our player. Which means it is more efficient to use
+     * FixedUpdate()
+     */
+    private void FixedUpdate()
+    {
+        /* Same as move by translate, we get our inputs
+         */
+        float force_horizontal = Input.GetAxis("Horizontal");
+        float force_vertical = Input.GetAxis("Vertical");
+
+        /* Then we create 3D vector consist of total force applied to our player.
+         */
+        Vector3 force = new Vector3(force_horizontal * force_applied, 0, force_vertical * force_applied);
+
+        /* Then apply the force to the rigidbody.
+         */
+        body.AddForce(force);
+
+        /* We now see benefits of using ridigbody and AddForce(); collision check is much more realistic;
+         * we don't have to worry about our player flying out from the map. Friction is applied to our
+         * player as well, allowing more smoother control over the player. However, since our player is
+         * 'rotating', our player's axis is not fixed; resulting unwanted behaviour in case we want to
+         * do certain actions that requires fixed axis, such as shooting or tracking.
+         */
+    }
+}

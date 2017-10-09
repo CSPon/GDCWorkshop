22396ed81298652fe8d3ffd388fc4770010e52ff
Charlie S. Aka CS <hsmchul@hotmail.com>
2017-10-08 19:55:49 -0500

    Added Scene #21

--- GDCProject/Assets/Session_10_to/Completed_Scenes/Completed_Scripts/Completed_IntermBarGUIBehaviour.cs
+++ GDCProject/Assets/Session_10_to/Completed_Scenes/Completed_Scripts/Completed_IntermBarGUIBehaviour.cs
@@ -2,15 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Completed_IntermBarGUIBehaviour : MonoBehaviour {
+/*
+ * GDC Intermediate Unity
+ *
+ * Bar guage
+ * 
+ */
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+[AddComponentMenu("GDC/Completed/Intermediate/GUIs/Bar Gauge")]
+public class Completed_IntermBarGUIBehaviour : MonoBehaviour
+{
+    private float minValue = 20;
+    private float maxValue = 600;
+    public float decreaseRate = 1;
+
+    public float decreaseInterval = 0.5f;
+    private float counter = 0;
+
+    private Transform bar;
+
+    private void Awake()
+    {
+        bar = this.transform;
+    }
+
+    private void UpdateBar()
+    {
+        Vector3 scale = bar.localScale;
+        scale.x -= decreaseRate;
+
+        if(scale.x > minValue)
+            bar.localScale = scale;
+        else
+        {
+            scale.x = minValue;
+            bar.localScale = scale;
+        }
+    }
+
+    private void Update()
+    {
+        if (counter > decreaseInterval)
+        {
+            UpdateBar();
+            counter = 0;
+        }
+        else counter += Time.deltaTime;
+    }
 }
